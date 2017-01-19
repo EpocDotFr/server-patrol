c8dc05594b50ce23864c509a477921defe545e47
EpocDotFr <contact@epoc.fr>
2017-01-19 13:33:57 +0100

    VEry first commit

---
+++ config.example.py
@@ -0,0 +1,5 @@
+SECRET_KEY = 'secretkeyhere'
+DEBUG = False
+LOGGER_HANDLER_POLICY = 'production'
+USER = {'username': 'password'}
+SMTP = {'LOGIN': 'hey', 'PASSWORD': 'hey', 'HOST': 'hey.com', 'PORT': '666', 'TLS': True}
---
+++ serverpatrol.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template
+from flask_httpauth import HTTPBasicAuth
+import logging
+import sys
+
+app = Flask(__name__, static_url_path='')
+app.config.from_pyfile('config.py')
+
+auth = HTTPBasicAuth()
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%d/%m/%Y %H:%M:%S',
+    stream=sys.stdout
+)
+
+logging.getLogger().setLevel(logging.INFO)
+
+# -----------------------------------------------------------
+
+
+@app.route('/')
+@auth.login_required
+def home():
+    return render_template('home.html')
+
+
+@app.route('/manage')
+@auth.login_required
+def manage():
+    return render_template('manage.html')
+
+
+@app.route('/rss')
+def rss():
+    return None
+
+
+# -----------------------------------------------------------
+
+
+@auth.get_password
+def get_password(username):
+    if username in app.config['USER']:
+        return app.config['USER'].get(username)
+
+    return None
