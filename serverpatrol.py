75c51503a51bff35bc80165d7c888f633fee65cf
EpocDotFr <contact@epoc.fr>
2018-01-31 08:50:21 +0100

    Put hooks in their own file

--- serverpatrol.py
+++ serverpatrol.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, make_response, g, request
 from logging.handlers import RotatingFileHandler
-from werkzeug.exceptions import HTTPException
 from flask_httpauth import HTTPBasicAuth
 from flask_sqlalchemy import SQLAlchemy
 from flask_babel import Babel
 from flask_mail import Mail
+from flask import Flask
 import logging
 import arrow
 
@@ -51,60 +50,4 @@ app.jinja_env.globals.update(arrow=arrow)
 import routes
 import models
 import commands
-
-
-# -----------------------------------------------------------
-# HTTP errors handler
-
-
-@app.errorhandler(401)
-@app.errorhandler(403)
-@app.errorhandler(404)
-@app.errorhandler(500)
-@app.errorhandler(503)
-def http_error_handler(error, without_code=False):
-    if isinstance(error, HTTPException):
-        error = error.code
-    elif not isinstance(error, int):
-        error = 500
-
-    body = render_template('errors/{}.html'.format(error))
-
-    if not without_code:
-        return make_response(body, error)
-    else:
-        return make_response(body)
-
-
-# -----------------------------------------------------------
-# Hooks
-
-
-@app.before_request
-def set_locale():
-    if not hasattr(g, 'CURRENT_LOCALE'):
-        if app.config['FORCE_LANGUAGE']:
-            g.CURRENT_LOCALE = app.config['FORCE_LANGUAGE']
-        else:
-            g.CURRENT_LOCALE = request.accept_languages.best_match(app.config['LANGUAGES'].keys(), default=app.config['DEFAULT_LANGUAGE'])
-
-
-@auth.get_password
-def get_password(username):
-    if username in app.config['USERS']:
-        return app.config['USERS'].get(username)
-
-    return None
-
-
-@auth.error_handler
-def auth_error():
-    return http_error_handler(403, without_code=True)
-
-
-@babel.localeselector
-def get_app_locale():
-    if not hasattr(g, 'CURRENT_LOCALE'):
-        return app.config['DEFAULT_LANGUAGE']
-    else:
-        return g.CURRENT_LOCALE
+import hooks
